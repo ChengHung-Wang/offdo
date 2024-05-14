03254c884b323cfaa86758656fe71e8cdb7e9486
ChengHung-Wang <apple1314520iphone@icloud.com>
2024-05-14 12:05:57 +0800

    Case Share Section(struct.)

---
+++ src/components/Home/CaseShare/CaseShareSection.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import StackingCards from "@/components/Home/CaseShare/StackingCards.vue";
+</script>
+
+<template>
+  <section id="case-share">
+    <StackingCards />
+  </section>
+</template>
+
+<style scoped lang="scss">
+
+</style>
