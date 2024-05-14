8daab0c2319e1a5a177d9addb80c1d21cc95ee57
ChengHung-Wang <apple1314520iphone@icloud.com>
2024-05-14 12:02:16 +0800

    pinia

---
+++ src/store/home-page-data.ts
@@ -0,0 +1,117 @@
+import { defineStore } from "pinia";
+
+export const useHomePageData = defineStore('homePageData', {
+    state: () => {
+        return {
+            plans: [
+                {
+                    title: "霹哩喀霹哩拉拉 酷酷告別式",
+                    price: "NT 80,000起",
+                    service: [
+                        "場地", "人員需求", "道具準備",
+                        "花卉", "場地佈置", "接待",
+                        "規劃流程", "道具準備", "其他..."
+                    ],
+                    hint: "和世界說掰掰就趁現在 ！ 想來一場主題派對嗎 ？"
+                },
+                {
+                    title: "波波力吶貝貝魯多 華麗下葬",
+                    price: "NT 10,000起",
+                    service: [
+                        "下葬方式", "大體處理", "道具準備",
+                        "墓地", "特殊壽衣", "音樂清單",
+                        "規劃流程", "其他..."
+                    ],
+                    hint: "想要穿著理想中的壽衣嗎 ？ 想在棺材中發"
+                },
+                {
+                    title: "嗶哩巴拉蹦 全都要葬禮",
+                    price: "NT 100,000起",
+                    service: [
+                        "場地", "人員需求", "大體處理",
+                        "花卉", "場地佈置", "特殊壽衣",
+                        "規劃流程", "接待", "靈車",
+                        "道具準備", "下葬方式", "禮車",
+                        "音樂清單", "墓地", "其他..."
+                    ],
+                    hint: "從告別式到下葬幫你一手包辦  。 來一場逼哩巴拉蹦的最後旅程吧 ！"
+                }
+            ],
+            cases: [
+                {
+                    title: "夜店PARTY灑骨灰",
+                    avatar: {
+                        name: "亡X宏",
+                        img: require("@/assets/images/avatar/casey-wang.png")
+                    },
+                    background: require("@/assets/images/cases/night-club.png"),
+                    message: "人生就是要嗨吧 ！ 夜店咚ㄘ咚ㄘ的精神\n" +
+                        "才是屬於我的世界 ， 我要把自己葬送在\n" +
+                        "這個極樂世界 ， 人生就是要開開心心地\n" +
+                        "結束啊 ！ 希望我的朋友們可以在我離開\n" +
+                        "的日子也能夠盡情的狂歡 。"
+                },
+                {
+                    title: "渣男前任PARTY",
+                    avatar: {
+                        name: "亡X宏",
+                        img: require("@/assets/images/avatar/casey-wang.png")
+                    },
+                    background: require("@/assets/images/cases/cry.png"),
+                    message: "說真的我人生其實沒什麼遺憾 ， 但我最\n" +
+                        "後悔的是曾經的我做過那些踐踏別人的\n" +
+                        "心的那些事 ， 我想要和我的前任們深深\n" +
+                        "地道歉 ， 我依然沒辦法正面的面對他們，\n" +
+                        "但我願意在最後向他們贖罪 ， 至少會讓\n" +
+                        "她們的心好過一點吧...\n" +
+                        "幸好有亡者計畫讓我能夠辦一個我想要\n" +
+                        "的後事。"
+                },
+                {
+                    title: "遺骨吉他",
+                    avatar: {
+                        name: "亡X宏",
+                        img: require("@/assets/images/avatar/casey-wang.png")
+                    },
+                    background: require("@/assets/images/cases/guitar.png"),
+                    message: "我一生奉獻於音樂 ， 從小就跟叔叔一起\n" +
+                        "玩各種樂器甚至一起組了樂團 ， 我們渴\n" +
+                        "望嗨翻全世界 ， 讓世界變得更活躍 ， 也\n" +
+                        "因此我也希望在我不在這個世界上的時\n" +
+                        "候能夠延續我的熱情 ， 所以就向亡者計\n" +
+                        "畫申請了服務 ， 我想要藉由我的遺骨製\n" +
+                        "作的吉他讓來到我的葬禮的人 ， 能享受\n" +
+                        "我的最後一份狂野 ， 讓大家記得我帶給\n" +
+                        "他們的熱情。"
+                }
+            ],
+            qa: [
+                {
+                    title: "如何聯繫亡者計劃以安排服務 ？",
+                    description: "你可以下載亡者計劃的app並填寫表單或是通過我們的網站上的聯繫表格或電話與我們聯繫 。 我們的客服團隊將會盡快與你取得聯繫 ， 安排適合你的諮詢時間 。",
+                    icon: require("@/assets/images/qa/q1.svg")
+                },
+                {
+                    title: "可以只客製化單項服務嗎 ？ 例如 : 特殊祭品 、 壽衣 。",
+                    description: "可以哦 ， 可以單獨項目獨立出來為你估價 ， 每個項目會依據不同的需求和細節衡量 ， 最後彙整給你 。 ",
+                    icon: require("@/assets/images/qa/q2.svg")
+                },
+                {
+                    title: "我可以估價再購買服務嗎？估價需要費用嗎？*抵銷",
+                    description: "可以哦 ， 但估價時會先跟你收一筆估價費 ， 費用為2000以內 ， 後續確認要購買服務後將會退回估價費用 。 ",
+                    icon: require("@/assets/images/qa/q3.svg")
+                },
+                {
+                    title: "非亡者本人可以預約嗎？*授權書 找律師",
+                    description: "可以哦 ， 但是要得到亡者本人的授權書或遺囑證明我們才能為你進行服務 。 ",
+                    icon: require("@/assets/images/qa/q4.svg")
+                },
+                {
+                    title: "取消服務可以退款嗎？",
+                    description: "取消服務是否可以退款通常取決於您所選擇的服務類型以及取消的時間 。 一般來說，如果您在服務開始之前取消 ， 您通常可以獲得全額退款或部分退款。但如果服務已經開始進行或完成 ， 則退款可能會受到限制 。",
+                    icon: require("@/assets/images/qa/q5.svg")
+                }
+            ]
+        }
+    }
+})
