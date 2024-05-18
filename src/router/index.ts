0340766a709fda2a355d3f6bba09669ee67b07cb
ChengHung-Wang <apple1314520iphone@icloud.com>
2024-05-19 04:16:05 +0800

    play ground router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PlayGroundView from '../views/PlayGrounds/PlayGroundView.vue'
+import UseIntersectionObserver from '../views/PlayGrounds/UseIntersectionObserver.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -14,6 +16,16 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/playground',
+    component: PlayGroundView,
+    children: [
+      {
+        path: '/use-intersection-observer',
+        component: UseIntersectionObserver
+      }
+    ]
   }
 ]
 
