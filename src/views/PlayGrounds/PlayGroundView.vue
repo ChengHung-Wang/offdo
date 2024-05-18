2e4c36a4da62135e53def048ece31ccf267d6428
ChengHung-Wang <apple1314520iphone@icloud.com>
2024-05-19 04:15:39 +0800

    play ground

---
+++ src/views/PlayGrounds/PlayGroundView.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class PlayGroundRoot extends Vue {}
+</script>
+
+<template>
+  <section id="play-ground">
+    <router-view />
+  </section>
+</template>
+
+<style scoped lang="scss">
+
+</style>
